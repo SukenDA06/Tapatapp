e1cf268c1a74c891f5f6b963a3d7059fbc4fb343
Soufian Bougrin <soufianbougrin44@gmail.com>
2025-02-25 14:27:49 +0100

    Reorganizacion de archivos

--- Python VC/Acts/Act1.py
+++
@@ -1,9 +0,0 @@
-print("hello world")
-
-num=3
-cadena="Text"
-
-print(num," és un tipus de dada:",type(num))
-print(cadena," és un tipus de dada:",type(cadena))
-num=3.1416
-print(num, " és un tipus de dada: " ,type(num))
--- Python VC/Acts/Act3.py
+++
@@ -1,8 +0,0 @@
-print('parell o senar')
-num = int(input())
-if (num % 2 == 0):
-    print(num, "És parell")
-else:
-    print("es senar")
-
-print("Finalitzar")
